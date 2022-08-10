0e156785c61e45245d3f8746a35e580ca1154493
Rodney Mupanduki <rodneymupanduki@gmail.co.zw>
2022-08-10 23:47:03 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
+import moment from 'moment'
 
 function App() {
+  const dates = []
+  data.map((date) => dates.push(date.Date))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table cellSpacing="0">
+        <thead>
+          <tr>
+            <td></td>
+            {
+              dates.map((data, i) => {
+                return (
+
+                  <td>{moment(data).format('dddd Do')}</td>
+
+                )
+              })
+            }
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>09:00 - 10:00</td>
+          </tr>
+          <tr>
+            <td>10:00 - 11:00</td>
+          </tr>
+          <tr>
+            <td>11:00 - 12:00</td>
+          </tr>
+          <tr>
+            <td>12:00 - 13:00</td>
+          </tr>
+          <tr>
+            <td>13:00 - 14:00</td>
+          </tr>
+
+          {/* {data.map((data, index) => {
+            return (
+              <tr>
+                <td> {data.HoursAvailable} </td>
+              </tr>
+            );
+          })} */}
+        </tbody>
+      </table>
     </div>
   );
 }
